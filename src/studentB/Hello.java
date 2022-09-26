aece80c4d0a85dfc9f168f404e334a1a585b15ce
Seo, Myoungjin <sih5673@gmail.com>
2022-09-26 14:10:47 +0900

    Hello 클래스 수정

--- src/studentB/Hello.java
+++ src/studentB/Hello.java
@@ -3,6 +3,12 @@ package studentB;
 public class Hello {
 	String message = "안녕하세요";
 
+	public Hello() {
+		this("안녕하세요");
+	}
+	public Hello(String message) {
+		this.message = message;
+	}
 	public void say() {
 		System.out.println(message);
 	}
